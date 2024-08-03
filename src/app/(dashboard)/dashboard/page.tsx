20bdefeeb4636b5255403aa92d58bf33e372e99d
Surya <suryakant.subudhi@outlook.com>
2024-08-03 13:29:24 +0530

    feat: cards in dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import { getUserAppointments } from '@/actions/appointment'
+import React from "react";
+import { getUserAppointments } from "@/actions/appointment";
 import {
   getUserBalance,
   getUserClients,
   getUserPlanInfo,
   getUserTotalProductPrices,
   getUserTransactions,
-} from '@/actions/dashboard'
-import DashboardCard from '@/components/dashboard/cards'
-import { PlanUsage } from '@/components/dashboard/plan-usage'
-import InfoBar from '@/components/infobar'
-import { Separator } from '@/components/ui/separator'
-import CalIcon from '@/icons/cal-icon'
-import PersonIcon from '@/icons/person-icon'
-import { TransactionsIcon } from '@/icons/transactions-icon'
-import { CreditCard, DollarSign } from 'lucide-react'
+} from "@/actions/dashboard";
+import DashboardCard from "@/components/dashboard/cards";
+import { PlanUsage } from "@/components/dashboard/plan-usage";
+import InfoBar from "@/components/infobar";
+import { Separator } from "@/components/ui/separator";
+import CalIcon from "@/icons/cal-icon";
+import PersonIcon from "@/icons/person-icon";
+import { ArrowUpRight, CreditCard, DollarSign } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Page = async () => {
-  const clients = await getUserClients()
-  const sales = await getUserBalance()
-  const bookings = await getUserAppointments()
-  const plan = await getUserPlanInfo()
-  const transactions = await getUserTransactions()
-  const products = await getUserTotalProductPrices()
+  const clients = await getUserClients();
+  const sales = await getUserBalance();
+  const bookings = await getUserAppointments();
+  const plan = await getUserPlanInfo();
+  const transactions = await getUserTransactions();
+  const products = await getUserTotalProductPrices();
 
   return (
     <div className="flex flex-col h-full">
@@ -54,47 +63,80 @@ const Page = async () => {
             />
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-            <div>
-              <h2 className="font-bold text-2xl">Plan Usage</h2>
-              <p className="text-sm font-light mb-4">
+            <Card>
+              <CardHeader>
+
+              <CardTitle className="font-bold text-2xl">Plan Usage</CardTitle>
+              <CardDescription className="text-sm font-light mb-4">
                 A detailed overview of your metrics, usage, customers and more
-              </p>
+              </CardDescription>
+              </CardHeader>
+              <CardContent>
               <PlanUsage
                 plan={plan?.plan!}
                 credits={plan?.credits || 0}
                 domains={plan?.domains || 0}
                 clients={clients || 0}
               />
-            </div>
-            <div className="flex flex-col">
-              <div className="w-full flex justify-between items-center mb-5">
-                <div className="flex gap-3 items-center">
-                  <TransactionsIcon />
-                  <p className="font-bold">Recent Transactions</p>
-                </div>
-                <p className="text-sm cursor-pointer hover:underline">See more</p>
-              </div>
-              <Separator orientation="horizontal" className="mb-5" />
-              {transactions &&
-                transactions.data.map((transaction) => (
-                  <div
-                    className="flex gap-3 w-full justify-between items-center border-b py-5"
-                    key={transaction.id}
-                  >
-                    <p className="font-semibold">
-                      {transaction.calculated_statement_descriptor}
-                    </p>
-                    <p className="font-bold text-xl">
-                      ${(transaction.amount / 100).toFixed(2)}
-                    </p>
+              </CardContent>
+            </Card>
+            <Card x-chunck="dashboard-01-chunck-03">
+              <CardHeader className="space-y-4">
+                <div className="flex flex-row items-center">
+                  <div className="grid gap-2">
+                    <CardTitle>Recent transactions</CardTitle>
+                    <CardDescription>
+                      Recent transactions from your Pipeline.
+                    </CardDescription>
                   </div>
-                ))}
-            </div>
+                  <Button
+                    asChild
+                    size="sm"
+                    className="ml-auto gap-1 bg-mayanjade hover:bg-aquamarine"
+                  >
+                    <Link href="#">
+                      View All
+                      <ArrowUpRight className="h-4 w-4" />
+                    </Link>
+                  </Button>
+                </div>
+                <Separator className="h-[0.5px] text-silvercloud left-4 right-4" />
+              </CardHeader>
+              <CardContent className="flex flex-col gap-4 mt-4">
+                {transactions &&
+                  transactions.data.map((transaction) => (
+                    <div
+                      className="flex items-center gap-4"
+                      key={transaction.id}
+                    >
+                      <Avatar className="hidden h-9 w-9 sm:flex">
+                        <AvatarFallback className="uppercase">
+                          {transaction.calculated_statement_descriptor?.slice(
+                            0,
+                            2
+                          )}
+                        </AvatarFallback>
+                      </Avatar>
+                      <div className="grid gap-1">
+                        <p className="text-sm font-medium leading-none">
+                          {transaction.calculated_statement_descriptor}
+                        </p>
+                        <p className="text-sm text-muted-foreground">
+                          {transaction.receipt_email}
+                        </p>
+                      </div>
+                      <div className="ml-auto font-medium">
+                        ${(transaction.amount / 100).toFixed(2)}
+                      </div>
+                    </div>
+                  ))}
+              </CardContent>
+            </Card>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
